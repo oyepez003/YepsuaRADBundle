b79e496a54bd2c19a94b29d16a14e89031048e11
Omar Yepez <oyepez003@gmail.com>
2015-03-17 16:58:57 -0430

    [new] New RAD for the CRUD Generator 2.0

--- Controller/CRUDController.php
+++ Controller/CRUDController.php
@@ -11,11 +11,277 @@
 
 namespace Yepsua\RADBundle\Controller;
 
-use Yepsua\RADBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class CRUDController extends Controller
+use Yepsua\RADBundle\Controller\Controller;
+use Yepsua\CommonsBundle\Persistence\Dao;
+use Yepsua\RADBundle\UI\Grid;
+
+use \YsJQuery as JQuery;
+use \YsJQueryConstant as JQueryConstant;
+use \YsGridResponse as GridResponse;
+
+abstract class CRUDController extends Controller
 {
-  public function dataAction(){
+    const NEW_VIEW = 'undefined';
+    const SHOW_VIEW = 'undefined';
     
-  }
+    const EDIT_VIEW = 'undefined';
+    const TRANSLATOR_DOMAIN = null;
+    
+    const ENTITY_NAME = 'undefined';
+    
+    const GRID_PARAM_NAME_ROWS = 'rows';
+    const GRID_PARAM_NAME_SIDX = 'sidx';
+    const GRID_PARAM_NAME_PAGE = 'page';
+    const GRID_PARAM_NAME_SORD = 'sord';
+    const GRID_PARAM_NAME_FILTERS = 'filters';
+    const ORDER_BY = 'ASC';
+    
+    const ERROR_HTTP_STATUS_CODE = 203;
+    
+    protected function crudIndex(Request $request = null){
+        $grid = $this->gridSettings();
+        return $this->commonsResponseVars(compact('grid'));
+    }
+    
+    protected function commonsResponseVars($vars = array()){
+      $translator_domain = static::TRANSLATOR_DOMAIN;
+      $entity_name = static::ENTITY_NAME;
+      return array_merge($vars, compact('translator_domain','entity_name'));
+    }
+    /**
+     * Creates a new entity.
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return type
+     */
+    protected function crudCreate(Request $request = null)
+    {
+        try{
+            
+            $entity = $this->getEntity();
+            $form = $this->createAndBindForm($request, $this->getEntityType() , $entity);
+            $page = static::NEW_VIEW;
+            
+            if ($form->isValid()) {
+                $this->saveEntity($entity);                
+                if($request->get('_loop_create')){
+                    $form = $this->createForm($this->getEntityType(), $this->getEntity());
+                }else{
+                    $page = static::SHOW_VIEW;
+                    $form = $this->createDeleteForm($entity->getId());
+                }
+                $form = $form->createView();
+                return $this->render($page, $this->commonsResponseVars(compact('entity','form')));
+            }
+            $form = $form->createView();
+            return $this->success($page, $this->commonsResponseVars(compact('entity','form')));
+            
+        }catch(\Exception $e){
+          return $this->failure($e);
+        }
+    }
+    
+    /**
+     * Displays a form to create a new entity.
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return type
+     */
+    public function crudNew(Request $request = null)
+    {
+        try{
+            $entity = $this->getEntity();
+            $form   = $this->createForm($this->getEntityType(), $entity)
+                           ->createView();
+            
+            return $this->commonsResponseVars(compact('entity','form'));
+        }catch(\Exception $e){
+            return $this->failure($e);
+        }
+    }
+    
+    /**
+     * Finds and displays an entity.
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param type $id
+     * @return type
+     */
+    public function crudShow(Request $request, $id)
+    {
+        try{
+
+          $entity = $this->throwEntityNotFoundException($this->getRepository()->find($id));
+          $form = $this->createDeleteForm($id)->createView();
+          
+          return $this->commonsResponseVars(compact('entity','form'));
+        }catch(\Exception $e){
+           return $this->failure($e);
+        }
+    }
+    
+    /**
+     * Displays a form to edit an existing entity.
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param type $id
+     * @return type
+     */
+    public function crudEdit(Request $request, $id)
+    {
+        try{
+            $entity = $this->throwEntityNotFoundException($this->getRepository()->find($id));
+            $form = $this->createForm( $this->getEntityType() , $entity)->createView();
+            $delete_form = $this->createDeleteForm($id)->createView();
+            
+            return $this->commonsResponseVars(compact('entity','form','delete_form'));
+            
+        }catch(\Exception $e){
+          return $this->failure($e);
+        }
+    }
+    
+    /**
+     * Edits an existing entity.
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param type $id
+     * @return type
+     */
+    public function crudUpdate(Request $request, $id)
+    {
+        try{
+            $entity = $this->throwEntityNotFoundException($this->getRepository()->find($id));
+
+            $form = $this->createForm($this->getEntityType(), $entity);
+            $form->submit($request);
+            
+            $page = static::EDIT_VIEW;
+            
+            if ($form->isValid()) {
+                $page = static::SHOW_VIEW;
+                $this->saveEntity($entity);
+                $form = $this->createDeleteForm($id)->createView();
+                return $this->render($page, $this->commonsResponseVars(compact('entity','form')));
+            }
+            
+            $form = $form->createView();
+            $delete_form = $this->createDeleteForm($id)->createView();
+            return $this->success($page, $this->commonsResponseVars(compact('entity','form','delete_form')));
+            
+        }catch(\Exception $e){
+            return $this->failure($e);
+        }
+    }
+    
+    /**
+     * Deletes an entity.
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param type $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function crudDelete(Request $request, $id)
+    {
+        try{
+            $form = $this->createMultipleDeleteForm($request, $id);
+            
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $ids = $this->getIdsToDelete($id);
+                $entities = $this->getRepository()->findById($ids);
+
+                foreach ($entities as $entity){
+                    if($entity){
+                      $em->remove($entity);
+                    }
+                }
+                
+                $em->flush();
+            }
+            return new Response();
+        }catch(\Exception $e){
+          return $this->failure($e);
+        }
+    }
+    
+    /**
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param type $entityName
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function searchJSONData(Request $request, $entityName, $getAll = false)
+      {
+          try{            
+              JQuery::useComponent(JQueryConstant::COMPONENT_JQGRID);
+              $getAll = ($getAll === true) || ($request->get(static::GRID_PARAM_NAME_ROWS, 0) <= 0) ? true : false;
+              
+              $orderBy = $request->get(static::GRID_PARAM_NAME_SIDX);
+              
+              $page = ($getAll) ? 1 : $request->get(static::GRID_PARAM_NAME_PAGE, 1);
+              $rows = $request->get(static::GRID_PARAM_NAME_ROWS, 1);
+              $sord = $request->get(static::GRID_PARAM_NAME_SORD, static::ORDER_BY);
+              $filters = $request->get(static::GRID_PARAM_NAME_FILTERS, null);
+              $response = new GridResponse();
+
+              $repository = $this->getRepository();
+              $count = Dao::count($repository);
+
+              if($count > 0){
+                  $query = Dao::buildQuery($repository, $entityName, $orderBy, $sord, $filters);
+                  if(!$getAll){
+                    $query->setMaxResults($rows)->setFirstResult(($page - 1) * $rows);
+                  }
+                  $entities = $query->getQuery()->getResult();
+
+                  foreach ($entities as $entitie){
+                      $response->addGridRow($this->dataRowMapping($entitie));
+                  }
+              }
+
+              $totalRows = ($getAll) ? $page : $count / $rows;
+              $totalRows = is_real($totalRows) ? intval($totalRows) + 1 : intval($totalRows);
+              $response->setTotal($totalRows);
+              $response->setPage($page);
+              $response->setRecords($count);
+
+              return new Response($response->buildResponseAsJSON());
+          }catch(\Exception $e){
+              return $this->failure($e);
+          }
+    }
+    
+    protected function getGrid(){
+      $grid = new Grid(static::ENTITY_NAME, 'list.view.grid.title');
+      $grid->setUrl($this->generateUrl(sprintf('%1$s_data', static::ENTITY_NAME)));
+      $grid->setTranslator($this->get('translator'), static::TRANSLATOR_DOMAIN);
+      $grid->setSortOrder(static::ORDER_BY);
+      $grid->createView();
+      return $grid;
+    }
+        
+    /**
+     * 
+     */
+    protected abstract function gridSettings();
+    
+    /**
+     * 
+     */
+    protected abstract function dataRowMapping($entitie);
+    
+    /**
+     * 
+     */
+    protected abstract function getEntity();
+    
+    /**
+     * 
+     * @return \Symfony\Component\Form\AbstractType;
+     */
+    protected abstract function getEntityType();
 }
--- Controller/Controller.php
+++ Controller/Controller.php
@@ -13,7 +13,9 @@ namespace Yepsua\RADBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Form;
 
 use Doctrine\DBAL\DBALException;
 
@@ -22,6 +24,8 @@ use Yepsua\SmarTwigBundle\UI\Message\Notification;
 class Controller extends BaseController
 {
     const REPOSITORY_NAMESPACE = 'undefined';
+    
+    const ERROR_HTTP_STATUS_CODE = 200;
     
     /**
      * Returns a NotFoundHttpException.
@@ -117,6 +121,18 @@ class Controller extends BaseController
     }
     
     /**
+     * 
+     * @param object $entity
+     * @throws NotFoundHttpException
+     */
+    public function throwEntityNotFoundException($entity = false){
+      if (!$entity) {
+        throw $this->createNotFoundException('msg.unable.to.find.entity');
+      }
+      return $entity;
+    }
+    
+    /**
      * Returns the Doctrine Entity Manager
      * @return type
      */
@@ -130,20 +146,20 @@ class Controller extends BaseController
      * @return \Symfony\Component\HttpFoundation\Response
      * @throws \Exception
      */
-    protected function exceptionManagerNotification(\Exception $ex){
+    protected function exceptionManagerNotification(\Exception $ex, $status = 200, $headers = array()){
       $response = null;
       try{
         throw $ex;
       }
       catch(NotFoundHttpException $nfhttpEx){
           $this->get('logger')->error($nfhttpEx->getMessage());
-          $response = $this->notifyError($nfhttpEx->getMessage());
+          $response = $this->notifyError($nfhttpEx->getMessage(), $status, $headers);
       }catch(DBALException $dbalEx){
           $this->get('logger')->error($dbalEx->getMessage());
-          $response = $this->notifyError($dbalEx->getPrevious()->getMessage());
+          $response = $this->notifyError($dbalEx->getPrevious()->getMessage(), $status, $headers);
       }catch(\Exception $e){
           $this->get('logger')->crit($e->getMessage());
-          $response = $this->notifyError($e->getMessage(), 403);
+          $response = $this->notifyError($e->getMessage(), 403, $headers);
       }
       return $response;
     }
@@ -213,5 +229,129 @@ class Controller extends BaseController
      */
     protected function notifyNotice($message, $status = 200, $headers = array()){
       return $this->notification($message, Notification::NOTICE_LEVEL, $status, $headers);
+    }
+    
+    /**
+     * 
+     * @param \Symfony\Component\Form\Form $form
+     * @return type
+     */
+    protected function getFormErrors(Form $form){
+      return $form->getErrors(true,true);
+    }
+    
+    /**
+     * 
+     * @param \Symfony\Component\Form\Form $form
+     * @return type
+     */
+    protected function throwFormException(Form $form){
+      return $this->throwException($this->getFormErrors($form));
+    }
+        
+    /**
+     * 
+     * @param type $message
+     * @throws \Exception
+     */
+    protected function throwException($message){
+      throw new \Exception($message);
+    }
+    
+    /**
+     * 
+     * @param \Symfony\Component\Form\Form $form
+     */
+    protected function validateForm(Form $form){
+      if(!$form->isValid()){
+        $this->throwFormException($form);
+      }
+    }
+    
+    /**
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param type $type
+     * @param type $data
+     * @param array $options
+     * @return type
+     */
+    protected function crateAndValidateForm(Request $request, $type, $data = null, array $options = array()){
+      $form = $this->createForm($type, $data, $options);
+      $form->submit($request);
+      $this->validateForm($form);
+      return $form;
+    }
+    
+    /**
+     * Creates, binds and returns a Form instance from the type of the form.
+     *
+     * @param string|FormTypeInterface $type    The built type of the form
+     * @param mixed                    $data    The initial data for the form
+     * @param array                    $options Options for the form
+     *
+     * @return Form
+     */
+    public function createAndBindForm(Request $request, $type, $data = null, array $options = array())
+    {
+      $form = $this->createForm($type, $data, $options);
+      $form->submit($request);
+      return $form;
+    }
+    
+    
+    /**
+     * 
+     * @param type $entity
+     */
+    protected function saveEntity($entity){
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($entity);
+      $em->flush();
+    }
+    
+    /**
+     * 
+     * @param type $e
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function handleException($e){
+      self::getLogger()->crit($e->getMessage());
+      return $this->exceptionManagerNotification($e, static::ERROR_HTTP_STATUS_CODE);
+    }
+    
+    /**
+     * 
+     * @param type $e
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function failure($e){
+      return $this->handleException($e);
+    }
+    
+    /**
+     * 
+     * @return type
+     */
+    protected final function getLogger(){
+      return $this->get('logger');
+    }
+    
+    protected function createMultipleDeleteForm(Request $request, $id){
+      if(strtoupper($request->getMethod()) == "DELETE"){
+        $form = $this->createDeleteForm($id);
+      }else{
+        $form = $this->createDeleteForm($id,array('csrf_protection' => false));
+      }
+      $form->submit($request);
+      return $form;
+    }
+    
+    protected function getIdsToDelete($id){
+       return strpos($id,',') ? explode(',', $id) : array($id);
+    }
+    
+    protected function success($view, array $parameters = array(), Response $response = null){
+      return $this->render($view,$parameters,new Response(null, 202));
     }
 }
